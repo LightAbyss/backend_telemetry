6890f2befe161ff1c5e28c7c12722399e4e6fb7e
Juan Pablo Corredor <juan.corredorie@gmail.com>
2026-03-21 21:46:00 +0100

    Creada función de validación de parámetros para DeviceCreate

--- app/api/schemas/device.py
+++ app/api/schemas/device.py
@@ -1,9 +1,24 @@
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, field_validator
 
 class DeviceCreate(BaseModel):
     device_id: str = Field(..., min_length=1)
     firmware_version: str = Field(..., min_length=1)
     device_type: str = Field(..., min_length=1)
+
+    @field_validator("device_id", "firmware_version", "device_type", mode="before")
+    def normalize_and_validate_string(cls, value) -> str:
+        if value is None:
+            raise ValueError("Must not be null")
+        
+        if not isinstance(value, str):
+            raise ValueError("Must be a string")
+        
+        new_value = value.strip()
+
+        if len(new_value) > 0:
+            return new_value
+        else:
+            raise ValueError("Must not be empty or whitespace")
 
 class DeviceCreateResponse(BaseModel):
     id: str
