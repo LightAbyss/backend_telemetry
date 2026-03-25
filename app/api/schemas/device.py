3bd96bd48d0d66130a1dcaf98779c27f88799513
Juan Pablo Corredor <juan.corredorie@gmail.com>
2026-03-25 11:31:00 +0100

    Añadida validación del formato para firmware_version

--- app/api/schemas/device.py
+++ app/api/schemas/device.py
@@ -1,12 +1,13 @@
 from pydantic import BaseModel, Field, field_validator
+import re
 
 class DeviceCreate(BaseModel):
     device_id: str = Field(..., min_length=1)
-    firmware_version: str = Field(..., min_length=1)
+    firmware_version: str = Field(...)
     device_type: str = Field(..., min_length=1)
     description: str | None = Field(default=None)
 
-    @field_validator("device_id", "firmware_version", "device_type", mode="before")
+    @field_validator("device_id", "device_type", mode="before")
     def normalize_and_validate_string(cls, value) -> str:
         if value is None:
             raise ValueError("Must not be null")
@@ -20,6 +21,24 @@ class DeviceCreate(BaseModel):
             return new_value
         else:
             raise ValueError("Must not be empty or whitespace")
+
+    @field_validator("firmware_version", mode="before")
+    def validate_firmware_version(cls, value) -> str:
+        if value is None:
+            raise ValueError("Must not be null")
+        
+        if not isinstance(value, str):
+            raise ValueError("Must be a string")
+        
+        new_value = value.strip()
+
+        if len(new_value) == 0:
+            raise ValueError("Must not be empty or whitespace")
+
+        if not re.match(r"^\d+\.\d+(\.\d+)?$", new_value):
+            raise ValueError("Must be in the format 'X.Y' or 'X.Y.Z' where X, Y, Z are numbers")
+        
+        return new_value
 
     @field_validator("description", mode="before")
     def normalize_description(cls, value) -> str | None:
